5c4fcfc56a1019dfccb2aad701a840c91f9c5caf
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-05 18:27:11 -0300

    :sparkles: import data from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useEffect, useState } from "react";
+import data from "./data.json";
+
 function App() {
   return (
     <>
       <h1>App</h1>
+      {data.map((country) => (
+        <p>{country.name}</p>
+      ))}
     </>
   );
 }
