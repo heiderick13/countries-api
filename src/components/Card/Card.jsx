f4a7b02a3591d2722c702223b0d2a1527357296a
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-05 19:31:55 -0300

    :lipstick: update card styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,11 +4,13 @@ function MovieCard({ country }) {
   return (
     <div className="country-card">
       <img className="country-img" src={country.flags.png} alt={country.name} />
-      <h3 className="country-title">{country.name}</h3>
+      <div className="country-info">
+        <p className="country-title">{country.name}</p>
 
-      <p className="population">Population: {country.population}</p>
-      <p className="region">Region: {country.region}</p>
-      <p className="capital">Capital: {country.capital}</p>
+        <p className="population">Population: {country.population}</p>
+        <p className="region">Region: {country.region}</p>
+        <p className="capital">Capital: {country.capital}</p>
+      </div>
     </div>
   );
 }
