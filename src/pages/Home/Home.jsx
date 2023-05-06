c9ea345cfe275c310e4211278077d22169a6296a
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-06 13:12:58 -0300

    :sparkles: search functionality

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,40 @@
 import "./Home.css";
 
+import { useState } from "react";
+
+import { MdSearch } from "react-icons/md";
+
 import data from "../../data.json";
 import Card from "../../components/Card/Card";
 
 function Home() {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="home">
-      {data.map((country) => (
-        <Card country={country} />
-      ))}
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search for a country"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <div className="countries-container">
+        {data
+          .filter((country) => {
+            return search.toLowerCase() === ""
+              ? country
+              : country.name.toLowerCase().includes(search);
+          })
+          .map((country) => (
+            <Card country={country} />
+          ))}
+      </div>
     </div>
   );
 }
+
+// cor de fundo: #202D36
 
 export default Home;
