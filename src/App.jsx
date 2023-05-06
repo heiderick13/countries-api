027ab956b4b03a25d19777a979afb99754495431
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-05-06 10:55:30 -0300

    import navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import Home from "./pages/Home/Home";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
